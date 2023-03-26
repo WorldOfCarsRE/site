02fd1b9466afee6af2e00348bd6c0f47ade3e584
Rocket <me@rocketprogrammer.me>
2023-03-25 20:02:26 -0500

    repo: Push

--- worldofcars/1.1/js/game.js
+++ worldofcars/1.1/js/game.js
@@ -1,17 +1,17 @@
 // Window mangaer
-var WMG = new windowManager( {
-	pages				: RDR,
-	omnitureTracking	: {
-		'purchase_sponsor_your_car_nav'		: { 'property' : 'car', 'account' : 'worldofcars', 'category' : 'dgame', 'site' : 'woc', 'siteSection' : 'game:sponsorship', 'contentType' : 'regular' },
-		'crews_news_landing_nav'			: { 'property' : 'car', 'account' : 'worldofcars', 'category' : 'dgame', 'site' : 'woc', 'siteSection' : 'game:community', 'contentType' : 'regular' },
-		'help_contact_us_report_a_bug_nav'	: { 'property' : 'car', 'account' : 'worldofcars', 'category' : 'dgame', 'site' : 'woc', 'siteSection' : 'game:community', 'contentType' : 'regular' },
-		'help_home_nav'						: { 'property' : 'car', 'account' : 'worldofcars', 'category' : 'dgame', 'site' : 'woc', 'siteSection' : 'game:community', 'contentType' : 'homepage' },
-		'change_server_nav'					: { 'property' : 'car', 'account' : 'worldofcars', 'category' : 'dgame', 'site' : 'woc', 'siteSection' : 'game:server', 'contentType' : 'regular' }
+var WMG = new windowManager({
+	pages: RDR,
+	omnitureTracking: {
+		'purchase_sponsor_your_car_nav': { 'property': 'car', 'account': 'worldofcars', 'category': 'dgame', 'site': 'woc', 'siteSection': 'game:sponsorship', 'contentType': 'regular' },
+		'crews_news_landing_nav': { 'property': 'car', 'account': 'worldofcars', 'category': 'dgame', 'site': 'woc', 'siteSection': 'game:community', 'contentType': 'regular' },
+		'help_contact_us_report_a_bug_nav': { 'property': 'car', 'account': 'worldofcars', 'category': 'dgame', 'site': 'woc', 'siteSection': 'game:community', 'contentType': 'regular' },
+		'help_home_nav': { 'property': 'car', 'account': 'worldofcars', 'category': 'dgame', 'site': 'woc', 'siteSection': 'game:community', 'contentType': 'homepage' },
+		'change_server_nav': { 'property': 'car', 'account': 'worldofcars', 'category': 'dgame', 'site': 'woc', 'siteSection': 'game:server', 'contentType': 'regular' }
 	}
-} );
+});
 
 // Omniture page tracking
-if( window.CTO ) {
+if (window.CTO) {
 	var cto = new CTO();
 	cto.account = 'worldofcars';
 	cto.category = 'dgame';
@@ -23,62 +23,62 @@ if( window.CTO ) {
 	cto.track();
 }
 
-jQuery( document ).ready( function() {
-	function displayGate( gateMsg ) {
-		jQuery( '#pla-gameModule' ).html( '<div class="pla-gateMessage">'+ gateMsg +'</div>' );
+jQuery(document).ready(function () {
+	function displayGate(gateMsg) {
+		jQuery('#pla-gameModule').html('<div class="pla-gateMessage">' + gateMsg + '</div>');
 	};
 
-	function embedFlash( bannerMsg ) {
+	function embedFlash(bannerMsg) {
 		var qsVars = {};
 		var pairs = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
 		var closingMessage = 'The World of Cars Online will be closing on February 8, 2012. ALL players can log into the online world and enjoy unlimited access for free through February 8, 2012.';
 
-		for(var i=0; i < pairs.length; i++) {
+		for (var i = 0; i < pairs.length; i++) {
 			var pair = pairs[i].split('=');
 			qsVars[pair[0]] = pair[1];
 		}
 
-		swfobject.embedSWF( PATH.cdnRoot +'/game/DVC_OS.swf?ver='+ clientVersion, 'pla-gameModule', '100%', '100%', '10.0.0', false, jQuery.extend( {}, GAME.flashVars, qsVars, { 'customMsg' : escape( ( bannerMsg || closingMessage ) ) } ), GAME.params );
+		swfobject.embedSWF(PATH.cdnRootBase + '/game/DVC_OS.swf?ver=' + clientVersion, 'pla-gameModule', '100%', '100%', '10.0.0', false, jQuery.extend({}, GAME.flashVars, qsVars, { 'customMsg': escape((bannerMsg || closingMessage)) }), GAME.params);
 	};
 
 	function heartbeatTick() {
-		if( !this.heartbeatFails ) {
+		if (!this.heartbeatFails) {
 			this.heartbeatFails = 0;
 		}
-		jQuery.ajax( {
-			cache : false,
-			dataType : 'text', // Previously xml type but our api service returns text
-			type : 'GET',
-			url : CFG.whoAmI,
-			error : function() {
-				if( heartbeatFails > 2 ) {
-					displayGate( 'The World of Cars Online is currently unavailable. We apologize for the inconvenience and are working quickly to resolve the problem. Please try again soon and thank you for your patience!' );
+		jQuery.ajax({
+			cache: false,
+			dataType: 'text', // Previously xml type but our api service returns text
+			type: 'GET',
+			url: CFG.whoAmI,
+			error: function () {
+				if (heartbeatFails > 2) {
+					displayGate('The World of Cars Online is currently unavailable. We apologize for the inconvenience and are working quickly to resolve the problem. Please try again soon and thank you for your patience!');
 					return false;
 				}
 
 				heartbeatFails = heartbeatFails + 1;
 				heartbeatTick();
 			},
-			success	: function( xml ) {
+			success: function (xml) {
 				heartbeatFails = 0;
-				if( typeof xml == 'object' || xml != false ) {
-					if( jQuery('testUser',xml).length > 0 && jQuery.inArray( jQuery('testUser',xml).text(), [ 'true', 'false' ] ) == -1 ) {
-						displayGate( jQuery('testUser',xml).text() );
-					} else if( jQuery('notification',xml).length > 0 && jQuery.inArray( jQuery('notification',xml).text(), [ 'true', 'false' ] ) == -1 ) {
-						embedFlash( jQuery('notification',xml).text() );
-					} else if( jQuery('gameClosed',xml).length > 0 && jQuery.inArray( jQuery('gameClosed',xml).text(), [ 'true', 'false' ] == -1 ) ) {
-						displayGate( jQuery('gameClosed',xml).text() );
+				if (typeof xml == 'object' || xml != false) {
+					if (jQuery('testUser', xml).length > 0 && jQuery.inArray(jQuery('testUser', xml).text(), ['true', 'false']) == -1) {
+						displayGate(jQuery('testUser', xml).text());
+					} else if (jQuery('notification', xml).length > 0 && jQuery.inArray(jQuery('notification', xml).text(), ['true', 'false']) == -1) {
+						embedFlash(jQuery('notification', xml).text());
+					} else if (jQuery('gameClosed', xml).length > 0 && jQuery.inArray(jQuery('gameClosed', xml).text(), ['true', 'false'] == -1)) {
+						displayGate(jQuery('gameClosed', xml).text());
 					} else {
 						embedFlash();
 					}
 				}
 			}
-		} );
+		});
 	}
 
-	if( !swfobject.hasFlashPlayerVersion( '10' ) ) {
+	if (!swfobject.hasFlashPlayerVersion('10')) {
 		window.location = 'flash-upgrade.html';
 	}
 
 	heartbeatTick();
-} );
+});
--- worldofcars/1.1/js/login.js
+++ worldofcars/1.1/js/login.js
@@ -1,5 +1,5 @@
 // Omniture page tracking
-if( window.CTO ) {
+if (window.CTO) {
 	var cto = new CTO();
 	cto.account = 'worldofcars';
 	cto.category = 'dgame';
@@ -12,75 +12,75 @@ if( window.CTO ) {
 }
 
 // Window mangaer
-var WMG = new windowManager( {
-	pages : RDR
-} );
+var WMG = new windowManager({
+	pages: RDR
+});
 
 
-jQuery( document ).ready( function() {
-	function displayGate( gateMsg ) {
-		jQuery( '#pla-regModule' ).html( '<div class="pla-gateMessage">'+ gateMsg +'</div>' );
+jQuery(document).ready(function () {
+	function displayGate(gateMsg) {
+		jQuery('#pla-regModule').html('<div class="pla-gateMessage">' + gateMsg + '</div>');
 	};
 
-	function embedFlash( bannerMsg ) {
+	function embedFlash(bannerMsg) {
 		// media mind tracking cookie
-		var	ebSession = MMD.getSessionID();
-		var ebRand = Math.random()+'';
+		var ebSession = MMD.getSessionID();
+		var ebRand = Math.random() + '';
 		ebRand = ebRand * 1000000;
 
 		var qsVars = {};
 		var pairs = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
 
-		for(var i=0; i < pairs.length; i++) {
+		for (var i = 0; i < pairs.length; i++) {
 			var pair = pairs[i].split('=');
 			qsVars[pair[0]] = pair[1];
 		}
 
-		swfobject.embedSWF( PATH.swf + '/common/DVC_OS.swf', 'pla-regModule', '100%', '100%', '10.0.0', false, jQuery.extend( { mediaMindConversion : 'HTTPS://bs.serving-sys.com/BurstingPipe/activity3.swf?' + escape('ebAS=bs.serving-sys.com&activityParams=ActivityID=73484&f=1&Session=' + ebSession + '&rnd=' + ebRand) }, RAMP.registration.flashVars, qsVars ), RAMP.registration.params );
+		swfobject.embedSWF(PATH.swf + '/common/DVC_OS.swf', 'pla-regModule', '100%', '100%', '10.0.0', false, jQuery.extend({ mediaMindConversion: 'HTTPS://bs.serving-sys.com/BurstingPipe/activity3.swf?' + escape('ebAS=bs.serving-sys.com&activityParams=ActivityID=73484&f=1&Session=' + ebSession + '&rnd=' + ebRand) }, RAMP.registration.flashVars, qsVars), RAMP.registration.params);
 	};
 
 	function heartbeatTick() {
-		if( !this.heartbeatFails ) {
+		if (!this.heartbeatFails) {
 			this.heartbeatFails = 0;
 		}
-		jQuery.ajax( {
-			cache : false,
-			dataType : 'text', // Previously xml type but our api service returns text
-			type : 'GET',
-			url : CFG.whoAmI,
-			error : function() {
-				if( heartbeatFails > 2 ) {
-					displayGate( 'The World of Cars Online is currently unavailable. We apologize for the inconvenience and are working quickly to resolve the problem. Please try again soon and thank you for your patience!' );
+		jQuery.ajax({
+			cache: false,
+			dataType: 'text', // Previously xml type but our api service returns text
+			type: 'GET',
+			url: CFG.whoAmI,
+			error: function () {
+				if (heartbeatFails > 2) {
+					displayGate('The World of Cars Online is currently unavailable. We apologize for the inconvenience and are working quickly to resolve the problem. Please try again soon and thank you for your patience!');
 					return false;
 				}
 
 				heartbeatFails = heartbeatFails + 1;
 				heartbeatTick();
 			},
-			success	: function( xml ) {
+			success: function (xml) {
 				heartbeatFails = 0;
-				if( typeof xml == 'object' || xml != false ) {
-					if( jQuery('testUser',xml).length > 0 && jQuery.inArray( jQuery('testUser',xml).text(), [ 'true', 'false' ] ) == -1 ) {
-						displayGate( jQuery('testUser',xml).text() );
-					} else if( jQuery('notification',xml).length > 0 && jQuery.inArray( jQuery('notification',xml).text(), [ 'true', 'false' ] ) == -1 ) {
-						embedFlash( jQuery('notification',xml).text() );
-					} else if( jQuery('gameClosed',xml).length > 0 && jQuery.inArray( jQuery('gameClosed',xml).text(), [ 'true', 'false' ] == -1 ) ) {
-						displayGate( jQuery('gameClosed',xml).text() );
+				if (typeof xml == 'object' || xml != false) {
+					if (jQuery('testUser', xml).length > 0 && jQuery.inArray(jQuery('testUser', xml).text(), ['true', 'false']) == -1) {
+						displayGate(jQuery('testUser', xml).text());
+					} else if (jQuery('notification', xml).length > 0 && jQuery.inArray(jQuery('notification', xml).text(), ['true', 'false']) == -1) {
+						embedFlash(jQuery('notification', xml).text());
+					} else if (jQuery('gameClosed', xml).length > 0 && jQuery.inArray(jQuery('gameClosed', xml).text(), ['true', 'false'] == -1)) {
+						displayGate(jQuery('gameClosed', xml).text());
 					} else {
 						embedFlash();
 					}
 				}
 			}
-		} );
+		});
 	}
 
-	if( !swfobject.hasFlashPlayerVersion( '10' ) ) {
+	if (!swfobject.hasFlashPlayerVersion('10')) {
 		window.location = 'flash-upgrade.html';
 	}
 
 	heartbeatTick();
-} );
+});
 
-function regComplete(){
-	mboxTrackLink( 'Cars_RegistrationComplete', false, 'game.html' );
+function regComplete() {
+	mboxTrackLink('Cars_RegistrationComplete', false, 'game.html');
 }
